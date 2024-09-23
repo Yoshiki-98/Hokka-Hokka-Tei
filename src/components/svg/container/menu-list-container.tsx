ecac1c4346b2df182c458d0fb54afb98213e15e0
Yoshiki-98 <yoshiyoshiyoshikirin27182828@gmail.com>
2024-09-24 08:14:38 +0900

    Fix: adjust svg frame to menu cards

--- src/components/svg/container/menu-list-container.tsx
+++ src/components/svg/container/menu-list-container.tsx
@@ -1,4 +1,3 @@
-import { ContainerProps } from '@/types/element-prop';
 import {
   Box,
   styled,
@@ -7,14 +6,10 @@ import {
 import { useTheme } from '@mui/material/styles';
 import { Menu } from '@/types/menu';
 
-// const SVGContainer = styled(Box)({
-//   width: '380px',
-//   height: '311px',
-//   overflow: 'hidden'
-// });
-
 const SVGContainer = styled(Box)({
   maxWidth: '393px', // SVGの元の幅
+  height: '260px',
+  overflow: 'hidden'
 });
 
 const LgSVGContainer = styled(Box)({
@@ -37,7 +32,7 @@ const SmSVGContainer = styled(Box)({
 
 interface ThumbnailProp {
   item: Menu;
-} 
+}
 
 const MenuListContainer: React.FC<ThumbnailProp> = ({ item }) => {
   const theme = useTheme();
@@ -47,10 +42,16 @@ const MenuListContainer: React.FC<ThumbnailProp> = ({ item }) => {
 
   return (
     <SVGContainer>
-      <svg width="100%" height="100%" viewBox="0 0 393 260" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg">
+      <svg
+        width="100%"
+        height="100%"
+        viewBox="0 0 393 260"
+        preserveAspectRatio="xMidYMid meet"
+        xmlns="http://www.w3.org/2000/svg"
+      >
         <defs>
           <clipPath id="svgShape">
-          <path d="M376.856 238.354C376.196 242.981 372.419 246.533 367.76 246.909L205.546 259.996C204.934 260.046 204.318 260.038 203.707 259.975L45.3867 243.512C41.3392 243.091 37.9504 240.259 36.8168 236.351L13.7208 156.726C13.2481 155.096 13.1979 153.373 13.575 151.718L37.5278 46.6174C38.2568 43.4184 40.5088 40.7805 43.5539 39.5587L122.904 7.71924C124.089 7.24414 125.353 7 126.628 7L296.555 7C297.889 7 299.209 7.26688 300.438 7.78491L367.601 36.0899C370.538 37.3274 372.709 39.8903 373.446 42.9902L392.56 123.322C392.851 124.543 392.909 125.807 392.732 127.049L376.856 238.354Z" fill="#D84343"/>
+            <path d="M376.856 238.354C376.196 242.981 372.419 246.533 367.76 246.909L205.546 259.996C204.934 260.046 204.318 260.038 203.707 259.975L45.3867 243.512C41.3392 243.091 37.9504 240.259 36.8168 236.351L13.7208 156.726C13.2481 155.096 13.1979 153.373 13.575 151.718L37.5278 46.6174C38.2568 43.4184 40.5088 40.7805 43.5539 39.5587L122.904 7.71924C124.089 7.24414 125.353 7 126.628 7L296.555 7C297.889 7 299.209 7.26688 300.438 7.78491L367.601 36.0899C370.538 37.3274 372.709 39.8903 373.446 42.9902L392.56 123.322C392.851 124.543 392.909 125.807 392.732 127.049L376.856 238.354Z" fill="#D84343"/>
           </clipPath>
         </defs>
         <image
