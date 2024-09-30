717c5fc8eef5361490e976979d94612d30e62323
Yoshiki-98 <yoshiyoshiyoshikirin27182828@gmail.com>
2024-10-01 00:08:11 +0900

    Finetune store list ui

--- src/components/store/store-list.tsx
+++ src/components/store/store-list.tsx
@@ -441,7 +441,7 @@ export default function StoreList() {
                   {stores && stores.length > 0 && (
                     <Typography
                       sx={{
-                        margin: '0 auto 20px auto',
+                        margin: {xs: '0 10px', sm: '0 auto 20px auto'},
                         textAlign: {sm: 'center', md: 'unset'},
                       }}
                       className="text-black items-center align-center"
