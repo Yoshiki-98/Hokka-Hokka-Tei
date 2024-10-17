bb7340805da4a0750a66fe36d811240985969f59
Yoshiki-98 <yoshiyoshiyoshikirin27182828@gmail.com>
2024-10-18 01:24:09 +0900

    Add meta desc

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -2,9 +2,11 @@ import MenuList from '@/components/menu/menu-list';
 import type { Metadata } from "next";
 import "src/app/globals.css";
 
-export const metadata: Metadata = {
-  title: "メニューを見る | ほっかほっか亭",
-  description: "ほっかほっか亭（ほか弁）のメニュー一覧です。各地域のお弁当の紹介や、アレルギー・栄養成分の一覧などを掲載しています。",
+export const generateMetadata = async (): Promise<Metadata> => {
+  return {
+    title: "メニューを見る | ほっかほっか亭",
+    description: "ほっかほっか亭（ほか弁）のメニュー一覧です。各地域のお弁当の紹介や、アレルギー・栄養成分の一覧などを掲載しています。",
+  };
 };
 
 export default function Home() {
--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -2,9 +2,11 @@ import StoreList from 'src/components/store/store-list';
 import type { Metadata } from "next";
 import "src/app/globals.css";
 
-export const metadata: Metadata = {
-  title: "店舗情報｜ほっかほっか亭",
-  description: "ほっかほっか亭のWebサイト。企業情報、店舗案内、フランチャイズ情報や、お問い合せ連絡先を掲載しています。",
+export const generateMetadata = async (): Promise<Metadata> => {
+  return {
+    title: "店舗情報｜ほっかほっか亭",
+    description: "ほっかほっか亭のWebサイト。企業情報、店舗案内、フランチャイズ情報や、お問い合せ連絡先を掲載しています。",
+  };
 };
 
 export default function Home() {
